677c5cb36700f781a1133aa15b27f6f27b21b7a2
Leo Zhang <leo@leozhang.me>
2021-09-10 23:30:17 -0700

    Update xmobar config
    - Use upstream xmobar now that #560 is merged
    - Fix HIE configuration
    - Don't show battery status on devices without a battery

--- private_dot_config/xmobar/xmobar.hs
+++ private_dot_config/xmobar/xmobar.hs
@@ -1,49 +1,103 @@
 module Main (main) where
 
-import Xmobar
 import Relude
+import System.Directory (doesFileExist)
+import Xmobar
 
 hasBattery :: IO Bool
-hasBattery = undefined
--- Check /sys/class/power_supply/BAT0/type == Battery
+hasBattery = getAny . mconcat <$> mapM isBattery batteryIDs
+ where
+  batteryIDs = ["BAT", "BAT0", "BAT1", "BAT2"]
+  isBattery batteryID = do
+    let f = "/sys/class/power_supply/" <> batteryID <> "/type"
+    exists <- doesFileExist f
+    Any
+      <$> if exists
+        then (== "Battery") <$> readFile "/sys/class/power_supply/BAT0/type"
+        else pure False
 
-hasWirelessInterface :: IO Bool
-hasWirelessInterface = undefined
 -- Check .../sys/class/net? `ip route`? `iw dev`?
 -- Tower has a wifi card... maybe I should look for "wifi + no ethernet"?
+hasWirelessInterface :: IO Bool
+hasWirelessInterface = undefined
+
 
 main :: IO ()
-main = xmobar $ defaultConfig {
-  font = "xft:monospace",
-  bgColor = "black",
-  fgColor = "grey",
-  position = TopW L 100,
-  commands = [
-      Run StdinReader,
-      Run $ Cpu ["-L", "3", "-H", "70", "--normal", "green", "--high", "red"] 10,
-      Run $ Memory ["-t", "Mem: <usedratio>%", "-L", "5", "-H", "70", "--normal", "green", "--high", "red"] 10,
-      Run $ DiskU [
-          ("/", "/: <used>/<size> (<usedp>%)"),
-          ("/home", "/home: <used>/<size> (<usedp>%)")
-        ]
-        ["-L", "5", "-H", "70", "--normal", "green", "--high", "red"] 10,
-      Run $ Volume "default" "Master" [] 1, -- Using `Alsa` won't always pick up when the default sink changes.
-      Run $ Alsa "default" "Capture" ["-t", "Mic: <volume>% <status>"],
-      Run $ Wireless "" [
-        "--Low", "55", "--High", "80",
-        "--low", "red", "--normal", "yellow", "--high", "green"
-      ] 10,
-      -- TODO: Automatically add or remove this by detecting whether this device has a battery?
-      Run $ Battery [
-        "--template", "<left>%<acstatus>, <timeleft> left",
-        "--Low", "40", "--High", "80",
-        "--low", "red", "--normal", "yellow", "--high", "green",
-        "--",
-        "-O", " (Charging)", "-i", " (Charged)", "-o", ""
-      ] 60,
-      Run $ Date "%k:%M %a %m/%d/%y" "datetime" 10
-    ],
-  sepChar = "%",
-  alignSep = "}{",
-  template = " %StdinReader% }{ %cpu% %memory% Disk: %disku% | %default:Master% %alsa:default:Capture% | %wi% | %battery% | %datetime% "
-}
+main = do
+  batteryPowered <- hasBattery
+  xmobar $
+    defaultConfig
+      { font = "xft:monospace"
+      , bgColor = "black"
+      , fgColor = "grey"
+      , position = TopW L 100
+      , commands =
+          [ Run StdinReader
+          , Run $ Cpu ["-L", "3", "-H", "70", "--normal", "green", "--high", "red"] 10
+          , Run $ Memory ["-t", "Mem: <usedratio>%", "-L", "5", "-H", "70", "--normal", "green", "--high", "red"] 10
+          , Run $
+              DiskU
+                [ ("/", "/: <used>/<size> (<usedp>%)")
+                , ("/home", "/home: <used>/<size> (<usedp>%)")
+                ]
+                ["-L", "5", "-H", "70", "--normal", "green", "--high", "red"]
+                10
+          , Run $ Volume "default" "Master" [] 1 -- Using `Alsa` won't always pick up when the default sink changes.
+          , Run $ Alsa "default" "Capture" ["-t", "Mic: <volume>% <status>"]
+          , Run $
+              Wireless
+                ""
+                [ "--Low"
+                , "55"
+                , "--High"
+                , "80"
+                , "--low"
+                , "red"
+                , "--normal"
+                , "yellow"
+                , "--high"
+                , "green"
+                ]
+                10
+          , Run $ Date "%k:%M %a %m/%d/%y" "datetime" 10
+          ]
+            ++ [ Run $
+                Battery
+                  [ "--template"
+                  , "<left>%<acstatus>, <timeleft> left"
+                  , "--Low"
+                  , "40"
+                  , "--High"
+                  , "80"
+                  , "--low"
+                  , "red"
+                  , "--normal"
+                  , "yellow"
+                  , "--high"
+                  , "green"
+                  , "--"
+                  , "-O"
+                  , " (Charging)"
+                  , "-i"
+                  , " (Charged)"
+                  , "-o"
+                  , ""
+                  ]
+                  60
+               | batteryPowered
+               ]
+      , sepChar = "%"
+      , alignSep = "}{"
+      , template =
+          " %StdinReader% }{ "
+            ++ intercalate
+              " | "
+              ( [ "%cpu% %memory% Disk: %disku%"
+                , "%default:Master% %alsa:default:Capture%"
+                , "%wi%"
+                ]
+                  ++ ["%battery%" | batteryPowered]
+                  ++ ["%datetime%"]
+              )
+            ++ " "
+      }
